c838e08ce9f825a7e5f0b2647a528b1911cb6350
frntprog <frntprog@gmail.com>
2024-02-29 20:25:49 +0200

    done m

---
+++ 189_Rotate_Array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  const rotations = k % nums.length;
+  if (rotations === 0) {
+    return nums;
+  }
+  const removedElements = nums.splice(nums.length - rotations);
+  nums.unshift(...removedElements);
+};
