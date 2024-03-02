e5b8e78fd0c9005b955fd3d3edfef2129f2d8730
frntprog <frntprog@gmail.com>
2024-03-02 23:18:57 +0200

    progress

---
+++ 121_Best_Time_to_Buy_and_Sell_Stock.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+// var maxProfit = function (prices) {
+//   let res =
+//     Math.max(
+//       ...prices.slice(prices.findIndex((el) => el == Math.min(...prices)) + 1)
+//     ) - Math.min(...prices);
+//   return res > 0 ? res : 0;
+// };
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function (prices) {};
+
+console.log(maxProfit([2, 4, 1]));
