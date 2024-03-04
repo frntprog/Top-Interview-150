72efc47872aab1bec86b93231e6f81a90634b3ce
frntprog <frntprog@gmail.com>
2024-03-04 23:43:12 +0200

    O(n)2

--- 121_Best_Time_to_Buy_and_Sell_Stock.js
+++ 121_Best_Time_to_Buy_and_Sell_Stock.js
@@ -5,15 +5,29 @@
 // var maxProfit = function (prices) {
 //   let res =
 //     Math.max(
-//       ...prices.slice(prices.findIndex((el) => el == Math.min(...prices)) + 1)
+//       ...prices.slice(
+//         prices.findIndex(
+//           (el) => el !== prices[prices.length - 1] && el == Math.min(...prices)
+//         )
+//       )
 //     ) - Math.min(...prices);
 //   return res > 0 ? res : 0;
 // };
 
-/**
- * @param {number[]} prices
- * @return {number}
- */
-var maxProfit = function (prices) {};
+var maxProfit = function (prices) {
+  let profit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = 1; j < prices.length; j++) {
+      if (prices[j] - prices[i] > profit) {
+        if (i < j) {
+          profit = prices[j] - prices[i];
+        }
+      }
+    }
+  }
+  return profit;
+};
 
+// console.log(maxProfit([4, 1, 5, 2, 7]));
 console.log(maxProfit([2, 4, 1]));
+// console.log(maxProfit([7, 1, 5, 3, 6, 4]));
