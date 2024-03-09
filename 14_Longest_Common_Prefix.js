19628602d265413ae92c15b27359be043c953630
frntprog <frntprog@gmail.com>
2024-03-09 18:36:14 +0200

    done

--- 14_Longest_Common_Prefix.js
+++ 14_Longest_Common_Prefix.js
@@ -2,24 +2,13 @@
  * @param {string[]} strs
  * @return {string}
  */
-var longestCommonPrefix = function (strs) {
-  if (strs[0].length == 0) return "";
-  let arr = strs.sort((a, b) => a.length - b.length);
-  let base = arr[0][0];
-  for (let j = 0; j < arr.length; j++) {
-    for (let i = 1; i < arr.length; i++) {
-      if (
-        arr[i].slice(0, base.length).includes(base) &&
-        arr[i + 1].includes(base + arr[i][i])
-      ) {
-        if (arr[i - 1].includes(base + arr[i][i])) {
-          console.log(arr[i]);
-          base += arr[i][i];
-        }
-      }
-    }
-  }
-  return base;
+var longestCommonPrefix = function (words) {
+  if (!words[0] || words.length == 1) return words[0] || "";
+  let i = 0;
+
+  while (words[0][i] && words.every((w) => w[i] === words[0][i])) i++;
+
+  return words[0].substr(0, i);
 };
 
 console.log(longestCommonPrefix(["abca", "aba", "aaab"]));
