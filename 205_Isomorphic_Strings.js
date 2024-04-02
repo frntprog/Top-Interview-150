12faf16033bb34756d559d0348ce5075e711a189
frntprog <frntprog@gmail.com>
2024-04-02 15:02:50 +0300

    done

--- 205_Isomorphic_Strings.js
+++ 205_Isomorphic_Strings.js
@@ -3,4 +3,23 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function (s, t) {};
+var isIsomorphic = function (s, t) {
+  let str = s.split("");
+  let ttr = t.split("");
+  let sMap = new Map();
+  let tMap = new Map();
+
+  for (let i = 0; i < str.length; i++) {
+    if (sMap.has(str[i]) && sMap.get(str[i]) !== ttr[i]) return false;
+    sMap.set(str[i], ttr[i]);
+  }
+
+  for (let i = 0; i < ttr.length; i++) {
+    if (tMap.has(ttr[i]) && tMap.get(ttr[i]) !== str[i]) return false;
+    tMap.set(ttr[i], str[i]);
+  }
+
+  return true;
+};
+
+console.log(isIsomorphic("egg", "add"));
