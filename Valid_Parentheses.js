eb4b7d03d79e0bab1ed92d48648d68e6fdbd88b3
frntprog <frntprog@gmail.com>
2024-04-04 13:11:21 +0300

    done

--- Valid_Parentheses.js
+++ Valid_Parentheses.js
@@ -3,13 +3,20 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-  const counts = { "(": 0, "[": 0, "{": 0 };
-  for (const char of s) {
-    if (char in counts) {
-      counts[char]++;
+  let stack = [];
+  const obj = { "(": ")", "[": "]", "{": "}" };
+  for (let i = 0; i < s.length; i++) {
+    if (obj[s[i]]) {
+      stack.push(s[i]);
+    } else {
+      const expectedClosingBracket = obj[stack.pop()];
+      if (s[i] !== expectedClosingBracket) {
+        return false;
+      }
     }
   }
-  return counts;
+
+  return stack.length == 0;
 };
 
-console.log(isValid("([]{}"));
+console.log(isValid("()"));
